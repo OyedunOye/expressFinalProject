ebb2ac705d1effe84713befa889c02da1836a247
OyedunOye <oyesinaoyedun@yahoo.com>
2024-03-11 23:05:10 +0100

    final touches added before assignment submission

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,6 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios');
 
 
 //Function to check if the user exists
@@ -33,45 +34,84 @@ public_users.post("/register", (req,res) => {
   }
 });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  res.send(JSON.stringify(books));
+// // Get the book list available in the shop, non-async. Async code style below
+// public_users.get('/',function (req, res) {
+//   //Write your code here
+//   res.send(JSON.stringify(books));
+// });
+
+//task 10
+public_users.get('/', async function (req, res) {
+  try {
+    res.send(JSON.stringify(books));
+    console.log("Promise for Task 10 resolved");
+
+  } catch(error) {
+    console.error('Error fetching books:', error);
+    res.status(500).send('Error fetching books');
+  }
+
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', async function (req, res) {
   //Write your code here
+
   let isbn = req.params.isbn
   let book = books[isbn]
-  res.send(book);
+  try {
+    if (book){
+    return res.status(200).send(book);
+    } else {
+      return res.status(400).send(`ISBN ${isbn} does not exist in the database!`)
+    }
+
+  } catch(error){
+    console.error(`Error fetching book with ISBN ${isbn}:`, error);
+    return res.status(500).send('Error fetching book');
+  }
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author',async function (req, res) {
   //Write your code here
-  const author = req.params.author
-  for (let i in books){
-    let book = books[i]
-    if (book["author"] == author) {
-      res.send(book)
-    }
-  // else{
-  //   return res.status(400).json({message: "Author does not exist in the database!"});
-  // }
-  }
   
+  try{
+    let author = req.params.author
+    
+    for (let i in books){
+      let book = books[i]
+      if (book["author"] == author) {
+        return res.status(200).send(book)
+      }
+    }
+    return res.status(400).json({message: `Author ${author} does not exist in the database!`});
+
+
+  }catch(error){
+    console.error(`Error fetching book written by author ${author}:`, error);
+    return res.status(500).send('Error fetching book.');
+  }
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title',async function (req, res) {
   //Write your code here
-  const title = req.params.title
+  let title = req.params.title
+  try{
+  
   for (let i in books){
     let book = books[i]
     if (book["title"] == title) {
-      res.send(book)
+      return res.status(200).send(book)
     }
+  }
+  return res.status(400).json({message: `Book titled ${title} does not exist in the database!`});
+
+  }
+  catch(error){
+    console.error(`Error fetching book titled ${title}:`, error);
+    return res.status(500).send('Error fetching book.');
   }
 });
 
@@ -79,7 +119,7 @@ public_users.get('/title/:title',function (req, res) {
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
   const book = req.params.isbn;
-  res.send(book["reviews"])
+  return res.status(200).send(book["reviews"])
 
 
 });
